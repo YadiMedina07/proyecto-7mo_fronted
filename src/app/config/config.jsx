747496455fc2902c1a7e85d175b79bf2e7e57687
Yadi-Medina <Yadi-Medina@example.com>
2024-10-25 14:04:13 -0600

    cambio de ruta

--- src/app/config/config.jsx
+++ src/app/config/config.jsx
@@ -1,5 +1,5 @@
 export const CONFIGURACIONES={
-    BASEURL:'https://proyecto-7mo-backend-pkho.vercel.app//api',
+    BASEURL:'https://proyecto-7mo-backend-pkho.vercel.app/api',
     // BASEURL:'http://localhost:4000/api'
 }
 
