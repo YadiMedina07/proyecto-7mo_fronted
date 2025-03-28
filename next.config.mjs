5de65a02f6cdc2e230edcfeb83e6d013d70893bc
Yadi-Medina <Yadi-Medina@example.com>
2025-03-28 15:13:15 -0600

    ''

--- next.config.mjs
+++ next.config.mjs
@@ -3,38 +3,32 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  
-  // Evita que se envíe "X-Powered-By: Next.js"
   poweredByHeader: false,
-  
   async headers() {
     return [
       {
         source: '/(.*)', // Aplica a todas las rutas
         headers: [
-          // Anti-Clickjacking
           {
             key: 'X-Frame-Options',
             value: 'DENY',
           },
-          // Evitar "mime sniffing"
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
-          // Control de caché
           {
             key: 'Cache-Control',
             value: 'no-store, no-cache, must-revalidate, private',
           },
-          // Content Security Policy
-          {
-            key: 'Content-Security-Policy',
-            value:
-              process.env.NODE_ENV === 'development'
-                ? "default-src 'self'; connect-src 'self' http://localhost:4000; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src https://www.google.com https://www.gstatic.com;"
-                : "default-src 'self'; connect-src 'self'; script-src 'self' 'unsafe-inline' https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src https://www.google.com https://www.gstatic.com;",
-          },
+          // Comentamos la cabecera CSP para desactivarla temporalmente:
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value:
+          //     process.env.NODE_ENV === 'development'
+          //       ? "default-src 'self'; connect-src 'self' http://localhost:4000; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src https://www.google.com https://www.gstatic.com;"
+          //       : "default-src 'self'; connect-src 'self'; script-src 'self' 'unsafe-inline' https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src https://www.google.com https://www.gstatic.com;",
+          // },
         ],
       },
     ];
