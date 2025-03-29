6f5463f146f1754f77eeaff5e27c62cd627fccaf
Yadi-Medina <Yadi-Medina@example.com>
2025-03-29 16:30:09 -0600

    actualizacion1

--- next.config.mjs
+++ next.config.mjs
@@ -21,13 +21,6 @@ const nextConfig = {
             key: 'Cache-Control',
             value: 'no-store, no-cache, must-revalidate, private',
           },
-          {
-            key: 'Content-Security-Policy',
-            value:
-              process.env.NODE_ENV === 'development'
-                ? "default-src 'self' data:; connect-src 'self' http://localhost:3000 http://localhost:4000 https://api.pwnedpasswords.com https://www.google.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://www.google.com/recaptcha/api2/clr; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src 'self' https://www.google.com https://www.gstatic.com;" // Agregado reCAPTCHA y localhost
-                : "default-src 'self' data:; connect-src 'self' https://api.pwnedpasswords.com https://www.google.com; script-src 'self' 'unsafe-inline' https://www.google.com https://www.gstatic.com https://www.google.com/recaptcha/api2/clr; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src 'self' https://www.google.com https://www.gstatic.com;", // Agregado reCAPTCHA
-          },
           // Comentamos la cabecera CSP para desactivarla temporalmente:
           // {
           //   key: 'Content-Security-Policy',
