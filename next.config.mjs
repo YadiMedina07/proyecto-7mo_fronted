43e36d77041a5a476200827747756f1d56e78874
Yadi-Medina <Yadi-Medina@example.com>
2025-03-30 11:15:13 -0600

    cambioCors

--- next.config.mjs
+++ next.config.mjs
@@ -23,14 +23,13 @@ const nextConfig = {
             key: 'Cache-Control',
             value: 'no-store, no-cache, must-revalidate, private',
           },
-          // Comentamos la cabecera CSP para desactivarla temporalmente:
-          // {
-          //   key: 'Content-Security-Policy',
-          //   value:
-          //     process.env.NODE_ENV === 'development'
-          //       ? "default-src 'self'; connect-src 'self' http://localhost:4000; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src https://www.google.com https://www.gstatic.com;"
-          //       : "default-src 'self'; connect-src 'self'; script-src 'self' 'unsafe-inline' https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src https://www.google.com https://www.gstatic.com;",
-          // },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              process.env.NODE_ENV === 'development'
+                ? "default-src 'self' data:; connect-src 'self' http://localhost:3000 https://proyecto-7mo-backend.onrender.com http://localhost:4000 https://api.pwnedpasswords.com https://www.google.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://www.google.com/recaptcha/api2/clr; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src 'self' https://www.google.com https://www.gstatic.com;" // Agregado reCAPTCHA y localhost
+                : "default-src 'self' data:; connect-src 'self' https://api.pwnedpasswords.com https://proyecto-7mo-backend.onrender.com https://www.google.com; script-src 'self' 'unsafe-inline' https://www.google.com https://www.gstatic.com https://www.google.com/recaptcha/api2/clr; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://res.cloudinary.com; frame-src 'self' https://www.google.com https://www.gstatic.com;", // Agregado reCAPTCHA
+          },
         ],
       },
     ];
